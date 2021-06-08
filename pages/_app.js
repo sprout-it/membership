e614c93937878e8e5a1e7fe89853b36909834c80
Fluke <jesadakorn.kirtnu@gmail.com>
2021-06-08 22:56:57 +0700

    add jsconfig

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import Layout from '../component/Layout'
-import '../styles/Login.css'
-import '../styles/Vertify.css'
+import Layout from 'component/Layout'
+import 'styles/Login.css'
+import 'styles/Vertify.css'
 import 'antd/dist/antd.min.css'
+
 const MyApp = ({ Component, pageProps }) => {
   return <Layout>
     <Component {...pageProps} />
