d07eaf067cd1f116cbf83369f2d015d680c50e8e
GuyAK10 <akkaraphol.khunpet@gmail.com>
2021-08-16 15:59:37 +0700

    footer layout responsive

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,80 @@
-import { Row, Col, Layout } from "antd"
+import { Row, Col, Layout, Typography } from "antd";
 
 const Footer = () => {
-    return <Layout.Footer justify='center' align='middle' style={{ width: '100%', backgroundColor: "#35b729", height: '100%' }}>
-        <Row justify="center" style={{ color: "#fff", height: "100%" }}>
-            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
-                {/* <Typography.Text style={{ fontFamily: 'Sukhumvit Set', color: '#fff', fontSize: 18 }}> All rights reserved. Copyright © 2021 Sprout Co., Ltd.</Typography.Text> */}
-                {/* <FooterComponent /> */}
-                <Row>Contact Information</Row>
-                <Row><img src="/images/footer/phone.svg" width={18} height={18} alt="phone" /><pre style={{ fontSize: 16 }}> 09 5517 9516</pre></Row>
-                <Row><img src="/images/footer/line.svg" width={18} height={18} alt="line" /><pre style={{ fontSize: 16 }}> @sproutcounter24</pre></Row>
-                <Row><img src="/images/footer/email.svg" width={18} height={18} alt="email" /><pre style={{ fontSize: 16 }}> admin@sproutstory.co</pre></Row>
-                <Row><img src="/images/footer/facebook.svg" width={18} height={18} alt="facebook" /><pre style={{ fontSize: 16 }}> https://www.facebook.com/sproutfoodT77/</pre></Row>
-            </Col>
-        </Row>
+  return (
+    <Layout.Footer
+      justify="center"
+      align="middle"
+      style={{ width: "100%", backgroundColor: "#35b729", height: "100%" }}
+    >
+      <Row
+        justify="center"
+        align="middle"
+        gutter={[0,0]}
+        wrap={true}
+        style={{ color: "#fff", height: "100%", alignItems: "center" }}
+      >
+        <Col xs={{span:22,offset:2}} sm={{span:22,offset:2}} md={{span:22,offset:2}} lg={{span:22,offset:2}} xl={{span:22,offset:2}} xxl={{span:22,offset:2}} style ={{width:'100%'}}>
+          <Row justify="start" align="middle" style={{padding:5,fontWeight:'bold',fontSize:20}}>
+            Contact Information
+          </Row>
+          <Row justify="start" align="middle" style={{padding:5}}>
+            <img
+              src="/images/footer/phone.svg"
+              width={18}
+              height={18}
+              alt="phone"
+            />
+            <Typography.Text
+              style={{ fontSize: 16, color: "#fff", marginLeft: 5 }}
+            >
+              {" "}
+              09 5517 9516
+            </Typography.Text>
+          </Row>
+          <Row justify="start" align="middle" style={{padding:5}}>
+            <img
+              src="/images/footer/line.svg"
+              width={18}
+              height={18}
+              alt="line"
+            />
+            <Typography.Text
+              style={{ fontSize: 16, color: "#fff", marginLeft: 5 }}
+            >
+              @sproutcounter24
+            </Typography.Text>
+          </Row>
+          <Row justify="start" align="middle" style={{padding:5}}>
+            <img
+              src="/images/footer/email.svg"
+              width={18}
+              height={18}
+              alt="email"
+            />
+            <Typography.Text
+              style={{ fontSize: 16, color: "#fff", marginLeft: 5 }}
+            >
+              {" "}
+              admin@sproutstory.co
+            </Typography.Text>
+          </Row>
+          <Row justify="start" align="middle" style={{padding:5}}>
+            <img
+              src="/images/footer/facebook.svg"
+              width={18}
+              height={18}
+              alt="facebook"
+            />
+            <Typography.Text
+              style={{ fontSize: 16, color: "#fff", marginLeft: 5 }}
+            >
+              https://www.facebook.com/sproutfoodT77/
+            </Typography.Text>
+          </Row>
+        </Col>
+      </Row>
     </Layout.Footer>
-}
-export default Footer
+  );
+};
+export default Footer;
