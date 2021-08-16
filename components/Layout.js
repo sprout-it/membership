10a87401cb6898d9ea44ee10bbec44697c4ec4aa
Fluke <jesadakorn.kirtnu@gmail.com>
2021-08-16 15:33:05 +0700

    add responsive navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,6 @@
 import React, { useContext } from "react";
 import { useRouter } from "next/router";
 import { Layout, Menu, Col, Row, Button, Typography, Card } from "antd";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import Header from './Header'
 import Footer from './Footer'
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,10 @@ import Head from 'next/head'
 import '../styles/globals.css'
 import '../styles/Login/Login.css'
 import '../styles/Vertify/Vertify.css'
-// import '../styles/Navbar/Navbar.css'
-// import '../styles/Navbar/Navbar425px.css'
-// import '../styles/Navbar/Navbar768px.css'
-// import '../styles/Navbar/Navbar1024px.css'
-// import '../styles/Navbar/Navbar1440px.css'
-// import '../styles/Navbar/Navbar1920px.css'
-// import '../styles/Navbar/Navbar2560px.css'
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import 'styles/Navbar/Navbar.css'
 import 'antd/dist/antd.min.css'
-import "@fortawesome/fontawesome-svg-core/styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { config } from "@fortawesome/fontawesome-svg-core";
 import LayoutComponent from '../components/Layout'
-config.autoAddCss = false;
 
 const MyApp = ({ Component, pageProps }) => {
   return (
